5fe1145c14a1faaba8f1bdee208d85425478b46f
Alexander Biryukov <alexander.biryukov@p2p.org>
2023-10-04 10:45:57 +0300

    logs

--- scripts/helpers/logger.ts
+++ scripts/helpers/logger.ts
@@ -2,13 +2,11 @@ import log4js from "log4js";
 
 log4js.configure({
     appenders: {
+        file: { type: "file", filename: 'logs.txt' },
         console: {
             type: 'console'
-        }
-    },
-    categories: {
-        default: { appenders: ['console'], level: 'info' }
-    }
+        } },
+    categories: { default: { appenders: ['console', "file"], level: "info" } },
 });
 
 export const logger = log4js.getLogger();
