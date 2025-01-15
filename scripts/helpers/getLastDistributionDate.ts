9a4361264671935ffeacd11bdf3034c2b97a6a80
alexander.biryukov <alexander.biryukov@p2p.org>
2025-01-15 16:20:25 +0400

    await sleep(5000)

--- scripts/helpers/getLastDistributionDate.ts
+++ scripts/helpers/getLastDistributionDate.ts
@@ -2,33 +2,41 @@ import { logger } from "./logger"
 import { getIsContract } from "./getIsContract"
 import { getFeeDistributorContract } from "./getFeeDistributorContract"
 import { ethers } from "ethers"
+import { sleep } from "./sleep"
 
 export async function getLastDistributionDate(fdAddress: string) : Promise<Date | null> {
-  logger.info('getLastDistributionDate started for ' + fdAddress)
+  try {
+    logger.info('getLastDistributionDate started for ' + fdAddress)
 
-  const isContract = await getIsContract(fdAddress)
-  if (!isContract) {
-    return null
+    const isContract = await getIsContract(fdAddress)
+    if (!isContract) {
+      return null
+    }
+
+    const fd = getFeeDistributorContract(fdAddress)
+
+    const logs = await fd.queryFilter(fd.filters.FeeDistributor__Withdrawn(), 19009010, "latest")
+
+    if (logs.length === 0) {
+      return null
+    }
+
+    const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL)
+
+    const withdrawalDates = await Promise.all(logs.map(async (log) => {
+      const block = await provider.getBlock(log.blockNumber)
+      return new Date(block.timestamp * 1000) // Convert Unix timestamp to JavaScript Date
+    }));
+
+    const maxDate = withdrawalDates.reduce((max, date) => date > max ? date : max)
+
+    logger.info('getLastDistributionDate finished for ' + fdAddress)
+
+    return maxDate
+  } catch (error) {
+    logger.error(error)
+    logger.info('sleeping for 5 sec...')
+    await sleep(5000)
+    return await getLastDistributionDate(fdAddress)
   }
-
-  const fd = getFeeDistributorContract(fdAddress)
-
-  const logs = await fd.queryFilter(fd.filters.FeeDistributor__Withdrawn(), 19009010, "latest")
-
-  if (logs.length === 0) {
-    return null
-  }
-
-  const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL)
-
-  const withdrawalDates = await Promise.all(logs.map(async (log) => {
-    const block = await provider.getBlock(log.blockNumber)
-    return new Date(block.timestamp * 1000) // Convert Unix timestamp to JavaScript Date
-  }));
-
-  const maxDate = withdrawalDates.reduce((max, date) => date > max ? date : max)
-
-  logger.info('getLastDistributionDate finished for ' + fdAddress)
-
-  return maxDate
 }
