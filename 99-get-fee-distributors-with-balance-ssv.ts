82cd4e11e1afc43e4b7690c2408f855902ac5345
alexander.biryukov <alexander.biryukov@p2p.org>
2025-03-12 16:42:27 +0400

    getNewFds ssv and all

--- 99-get-fee-distributors-with-balance-ssv.ts
+++ 99-get-fee-distributors-with-balance-ssv.ts
@@ -1,12 +1,63 @@
 import "dotenv/config"
 import {logger} from "./scripts/helpers/logger";
 import {getFeeDistributorsWithBalanceSsv} from "./scripts/getFeeDistributorsWithBalanceSsv";
+import { getFeeDistributorContract } from "./scripts/helpers/getFeeDistributorContract"
+import { NewFd } from "./scripts/models/NewFd"
+import { getDatedJsonFilePath } from "./scripts/helpers/getDatedJsonFilePath"
+import fs from "fs"
 
 async function main() {
     logger.info('99-get-fee-distributors-with-balance-ssv started')
 
     try {
-        const fds = await getFeeDistributorsWithBalanceSsv()
+        const {feeDistributorsWithBalance: fds, now} = await getFeeDistributorsWithBalanceSsv()
+        const currentFds = fds.filter(f => f.periods.some(p => p.endDate === now))
+
+        const newFds: NewFd[] = []
+
+        for(const currentFd of currentFds) {
+            const f = getFeeDistributorContract(currentFd.fdAddress)
+            try {
+                const client = await f.client()
+                const clientBasisPoints = (await f.clientBasisPoints()).toNumber()
+
+                const sameClientFd = newFds.find(
+                  nfd => nfd.clientAddress.toLowerCase() === client.toLowerCase()
+                )
+                if (sameClientFd) {
+                    sameClientFd.oldFeeDistributors.push(
+                      {
+                          address: currentFd.fdAddress,
+                          clientBasisPoints: clientBasisPoints,
+                          type: 'ssvNew'
+                      }
+                    )
+                } else {
+                    newFds.push({
+                        newFeeDistributorAddress: "",
+                        clientAddress: client,
+                        oldFeeDistributors: [
+                            {
+                                address: currentFd.fdAddress,
+                                clientBasisPoints: clientBasisPoints,
+                                type: 'ssvNew'
+                            }
+                        ]
+                    })
+                }
+
+            } catch (e){
+                console.error('Error in ' + currentFd.fdAddress)
+                console.error(e)
+            }
+        }
+
+
+        const filePath = getDatedJsonFilePath('newFdsSSV_OLD')
+        logger.info('Saving newFds to ' + filePath)
+        fs.writeFileSync(filePath, JSON.stringify(newFds))
+        logger.info('newFds saved')
+
     } catch (error) {
         logger.error(error)
     }
--- scripts/getFeeDistributorsWithBalanceSsv.ts
+++ scripts/getFeeDistributorsWithBalanceSsv.ts
@@ -1,6 +1,6 @@
 import {logger} from "./helpers/logger";
 import {getBalance} from "./helpers/getBalance";
-import {ethers} from "ethers";
+import { BigNumber, ethers } from "ethers"
 import {FeeDistributorToWithdraw} from "./models/FeeDistributorToWithdraw";
 import {getP2pSsvProxyAddresses} from "./ssv/getP2pSsvProxyAddresses";
 import {getP2pSsvProxyAddresses_3_1} from "./ssv/getP2pSsvProxyAddresses_3_1";
@@ -18,9 +18,9 @@ export async function getFeeDistributorsWithBalanceSsv() {
 
     const periods: {recipientAddress: string, from: Date, to: Date | null, pubkeys: string[]}[] = []
 
-    const proxyAddresses = await getP2pSsvProxyAddresses()
-    const proxyAddresses_3_1 = await getP2pSsvProxyAddresses_3_1()
-    proxyAddresses.push(...proxyAddresses_3_1)
+    // const proxyAddresses = await getP2pSsvProxyAddresses()
+    const proxyAddresses = await getP2pSsvProxyAddresses_3_1()
+    // proxyAddresses.push(...proxyAddresses_3_1)
 
     for (const proxyAddress of proxyAddresses) {
         const feeRecipientAddressesWithTimestamps = await getSsvFeeRecipientAddressesWithTimestampsPerProxy(proxyAddress)
@@ -73,24 +73,6 @@ export async function getFeeDistributorsWithBalanceSsv() {
     for (const period of periods) {
         try {
 
-            const balance = await getBalance(period.recipientAddress)
-            logger.info(
-                'Balance of '
-                + period.recipientAddress
-                + ' is '
-                + ethers.utils.formatUnits(balance, "ether")
-                + 'ETH'
-            )
-
-            if (balance.lt(ethers.utils.parseUnits(process.env.MIN_BALANCE_TO_WITHDRAW_IN_GWEI, "gwei"))) {
-                logger.info(
-                    'Balance of '
-                    + period.recipientAddress
-                    + ' is less than minimum to withdraw. Will not withdraw.'
-                )
-                continue
-            }
-
             const existingFd = feeDistributorsWithBalance.find(f => f.fdAddress === period.recipientAddress)
             const currentPeriod = {
                 pubkeys: period.pubkeys,
@@ -104,7 +86,7 @@ export async function getFeeDistributorsWithBalanceSsv() {
                 feeDistributorsWithBalance.push({
                     fdAddress: period.recipientAddress,
                     identityParams: null,
-                    balance,
+                    balance: BigNumber.from(0),
                     periods: [currentPeriod],
                     newClientBasisPoints: null,
                     amount: 0
@@ -116,5 +98,5 @@ export async function getFeeDistributorsWithBalanceSsv() {
     }
 
     logger.info('getFeeDistributorsWithBalanceSsv finished')
-    return feeDistributorsWithBalance
+    return {feeDistributorsWithBalance, now}
 }
